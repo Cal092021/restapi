48c9b7521aa5224a2c0f1a9f4f8888b20c37964b
Alex Calhoun <alxcalhoun@gmail.com>
2022-03-18 09:14:23 +0000

    update

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -10,3 +10,34 @@ exports.addUser = async (req, res) => {
     }
 
 };
+
+exports.deleteUser = async (req, res) => {
+    try{
+        const deleteUser = await User.deleteOne(req.body);
+        res.status(200).send({user: deleteUser});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try{
+        const updateUser = await User.updateOne(req.body);
+        res.status(200).send({user: updateUser});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try{
+        const updateUser = await User.updateOne(req.body);
+        res.status(200).send({user: updateUser});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
